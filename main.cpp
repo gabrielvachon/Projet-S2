562bdf7e2c1282467c67763d8bba15a6cff2d9ec
samuel <samueltrep@hotmail.com>
2022-02-27 15:50:59 -0500

    Auto stash before merge of "main" and "origin/main"

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <conio.h>
 #include "labyrinthe.h"
 #include "joueur.h"
+#include <string.h>
 
 
 using namespace std;
@@ -10,5 +11,32 @@ int main()
 {
     cout << "Testing" << endl;
     Labyrinthe unLabyrinthe(true);
+
+    char dir;
+    while((dir = getch()) != 'q')
+    {
+        switch (dir)
+        {
+        
+        case 'w':
+            
+            break;
+        case 'a':
+
+            break;
+        case 's':
+            
+            break;
+        case 'd':
+            
+            break;
+        
+        default:
+            break;
+
+            
+        }
+        
+    }
     return 0;
 }
